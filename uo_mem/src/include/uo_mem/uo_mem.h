0274cb0218a090bc4f0fbae478f3793bf2eee74f
Leevi Uotinen <uotinen.leevi@hotmail.com>
2018-12-28 10:45:13 +0200

    Add macro for appending null terminated strings

--- uo_mem/src/include/uo_mem/uo_mem.h
+++ uo_mem/src/include/uo_mem/uo_mem.h
@@ -10,6 +10,8 @@ extern "C" {
 #include <stdlib.h>
 #include <string.h>
 
+static _Thread_local size_t temp_size;
+
 #define uo_mem_write(dst, src, len) \
 { \
     const size_t UO_VAR(len_) = (len); \
@@ -17,8 +19,14 @@ extern "C" {
 }
 
 #define uo_mem_append_str_literal(dst, str_literal) _Generic((dst), \
-     char *: ((char *)memcpy((dst), (str_literal), UO_STRLEN(str_literal)) + UO_STRLEN(str_literal)), \
-    default: ((void *)((char *)memcpy((dst), (str_literal), UO_STRLEN(str_literal)) + UO_STRLEN(str_literal))))
+              char *: ((char *)memcpy((dst), (str_literal), UO_STRLEN(str_literal)) + UO_STRLEN(str_literal)), \
+     unsigned char *: ((unsigned char *)memcpy((dst), (str_literal), UO_STRLEN(str_literal)) + UO_STRLEN(str_literal)), \
+             default: ((void *)((char *)memcpy((dst), (str_literal), UO_STRLEN(str_literal)) + UO_STRLEN(str_literal))))
+
+#define uo_mem_append_str(dst, str) _Generic((dst), \
+              char *: ((char *)memcpy((dst), (str), temp_size = strlen(str)) + temp_size), \
+     unsigned char *: ((unsigned char *)memcpy((dst), (str), temp_size = strlen(str)) + temp_size), \
+             default: ((void *)((char *)memcpy((dst), (str), temp_size = strlen(str)) + temp_size)))
 
 #define uo_mem_using(p, size) \
     for (int UO_VAR(once) = 1; UO_VAR(once);) \
