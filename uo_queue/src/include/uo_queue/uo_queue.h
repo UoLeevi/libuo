85f0c659419a07eda5581fb234afb0505c3cf064
Leevi Uotinen <uotinen.leevi@hotmail.com>
2019-01-03 20:36:31 +0200

    Make the pthread dependency non public because it is implementation specific

--- uo_queue/src/include/uo_queue/uo_queue.h
+++ uo_queue/src/include/uo_queue/uo_queue.h
@@ -5,35 +5,33 @@
 extern "C" {
 #endif
 
-#include <semaphore.h>
-
 #include <stdatomic.h>
 #include <stddef.h>
 #include <stdbool.h>
 
 typedef struct uo_queue {
-	atomic_size_t head;
-	atomic_size_t tail;
+    void **items;
+    atomic_size_t head;
+    atomic_size_t tail;
     size_t capasity;
-	sem_t enqueue_sem;
-	sem_t dequeue_sem;
-	void **items;
+    void *enqueue_sem;
+    void *dequeue_sem;
 } uo_queue;
 
 uo_queue *uo_queue_create(
-	size_t capasity);
+    size_t capasity);
 
 void uo_queue_destroy(
-	uo_queue *);
+    uo_queue *);
 
 bool uo_queue_enqueue(
-	uo_queue *, 
-	void *item, 
-	const int blocking);
+    uo_queue *, 
+    void *item, 
+    bool should_block);
 
 void *uo_queue_dequeue(
-	uo_queue *, 
-	const int blocking);
+    uo_queue *, 
+    bool should_block);
 
 #ifdef __cplusplus
 }
--- uo_queue/src/uo_queue.c
+++ uo_queue/src/uo_queue.c
@@ -2,61 +2,64 @@
 
 #include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
+
+#include <semaphore.h>
 
 uo_queue *uo_queue_create(
-	size_t capasity)
+    size_t capasity)
 {
-	uo_queue *queue = calloc(1, sizeof *queue);
+    uo_queue *queue = calloc(1, sizeof *queue);
 
-	sem_init(&queue->enqueue_sem, 0, capasity);
-	sem_init(&queue->dequeue_sem, 0, 0);
+    sem_init(&queue->enqueue_sem, 0, capasity);
+    sem_init(&queue->dequeue_sem, 0, 0);
 
-	atomic_init(&queue->head, 0);
-	atomic_init(&queue->tail, 0);
+    atomic_init(&queue->head, 0);
+    atomic_init(&queue->tail, 0);
 
-	queue->capasity = capasity;
-	queue->items = malloc(sizeof *queue->items * capasity);
+    queue->capasity = capasity;
+    queue->items = malloc(sizeof *queue->items * capasity);
 
-	return queue;
+    return queue;
 }
 
 void uo_queue_destroy(
-	uo_queue *queue)
+    uo_queue *queue)
 {
-	free(queue->items);
-	free(queue);
+    free(queue->items);
+    sem_destroy(&queue->enqueue_sem);
+    sem_destroy(&queue->dequeue_sem);
+    free(queue);
 }
 
 bool uo_queue_enqueue(
-	uo_queue *queue, 
-	void *item, 
-	const int blocking)
+    uo_queue *queue, 
+    void *item, 
+    bool should_block)
 {
-	if (blocking)
-		sem_wait(&queue->enqueue_sem);
-	else if (sem_trywait(&queue->enqueue_sem) == -1)
-		return false;
+    if (should_block)
+        sem_wait(&queue->enqueue_sem);
+    else if (sem_trywait(&queue->enqueue_sem) == -1)
+        return false;
 
-	size_t head = atomic_fetch_add(&queue->head, 1);
-	queue->items[head % queue->capasity] = item;
-	sem_post(&queue->dequeue_sem);
+    size_t head = atomic_fetch_add(&queue->head, 1);
+    queue->items[head % queue->capasity] = item;
+    sem_post(&queue->dequeue_sem);
 
-	return true;
+    return true;
 }
 
 void *uo_queue_dequeue(
-	uo_queue *queue, 
-	const int blocking)
+    uo_queue *queue, 
+    bool should_block)
 {
-	if (blocking)
-		sem_wait(&queue->dequeue_sem);
-	else if (sem_trywait(&queue->dequeue_sem) == -1)
-		return NULL;
+    if (should_block)
+        sem_wait(&queue->dequeue_sem);
+    else if (sem_trywait(&queue->dequeue_sem) == -1)
+        return NULL;
 
-	size_t tail = atomic_fetch_add(&queue->tail, 1);
-	void *item = queue->items[tail % queue->capasity];
-	sem_post(&queue->enqueue_sem);
+    size_t tail = atomic_fetch_add(&queue->tail, 1);
+    void *item = queue->items[tail % queue->capasity];
+    sem_post(&queue->enqueue_sem);
 
-	return item;
+    return item;
 }
