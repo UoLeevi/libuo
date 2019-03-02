32991ffe7d7d4aba2f6d05ba293d4b81e6167b39
Leevi Uotinen <uotinen.leevi@hotmail.com>
2019-03-02 11:27:51 +0200

    Add documentation comments for uo_linklist

--- uo_linklist/src/include/uo_linklist/uo_linklist.h
+++ uo_linklist/src/include/uo_linklist/uo_linklist.h
@@ -11,8 +11,15 @@ extern "C" {
 /**
  * @brief list for linking memory addresses
  * 
- * Use this struct as the first member of another struct to make the parent a list link.
- * To access the parent stuct, just convert the uo_linklist pointer to parent type pointer.
+ * Use this struct as the first member of another struct to make the parent 
+ * struct a "link" that can be used in a linklist. To access the parent struct, 
+ * just convert the uo_linklist pointer to a pointer of the parent type.
+ * 
+ * All uo_linklist structs should be zero initialized before use.
+ * uo_linklist next and prev are NULL when they do point to another link.
+ * i.e. Empty linklist has next and prev set to NULL.
+ * 
+ * Circular linklists are permitted but linklist should never be inserted to itself.
  */
 typedef struct uo_linklist
 {
@@ -20,24 +27,45 @@ typedef struct uo_linklist
     struct uo_linklist *prev;
 } uo_linklist;
 
+/**
+ * @brief get next link
+ * 
+ */
 #define uo_linklist_next(link) \
     ((void *)(((uo_linklist *)(link))->next))
 
+/**
+ * @brief get previous link
+ * 
+ */
 #define uo_linklist_prev(link) \
     ((void *)(((uo_linklist *)(link))->prev))
 
+/**
+ * @brief zero initialize a link
+ * 
+ */
 #define uo_linklist_reset(link) \
     uo__linklist_reset((uo_linklist *)(link))
 
-#define uo_linklist_insert(pos, link) \
-    uo__linklist_insert((uo_linklist *)(pos), (uo_linklist *)(link))
-
+/**
+ * @brief insert link after position marked by another link
+ * 
+ */
 #define uo_linklist_insert_before(pos, link) \
     uo__linklist_insert_before((uo_linklist *)(pos), (uo_linklist *)(link))
 
+/**
+ * @brief insert link before position marked by another link
+ * 
+ */
 #define uo_linklist_insert_after(pos, link) \
     uo__linklist_insert_after((uo_linklist *)(pos), (uo_linklist *)(link))
 
+/**
+ * @brief remove link from linklist
+ * 
+ */
 #define uo_linklist_remove(link) \
     uo__linklist_remove((uo_linklist *)(link))
 
